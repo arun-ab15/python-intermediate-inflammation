fad8fe3cb8327b5a9d58cedfec0738df638786a5
Arun <arunbalaji.balanrangaraj@astrazeneca.com>
2022-10-12 17:38:26 +0530

    Adding patient normalise method

--- tests/test_models.py
+++ tests/test_models.py
@@ -39,3 +39,34 @@ def test_daily_min(test, expected):
     """Test that max function works for an array of positive integers."""
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(test), expected)
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+                'hello',
+                None,
+                TypeError,
+        ),
+        (
+                3,
+                None,
+                TypeError,
+        ),
+        (
+                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                None,
+        )
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    if isinstance(test, list):
+        test = np.array(test)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
